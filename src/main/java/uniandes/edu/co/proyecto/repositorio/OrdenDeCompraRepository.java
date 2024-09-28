ec1a509b9c026286140f751516709f30727eedb3
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2024-09-28 12:54:59 -0500

    seso

--- src/main/java/uniandes/edu/co/proyecto/repositorio/OrdenDeCompraRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/OrdenDeCompraRepository.java
@@ -1,33 +1,15 @@
 package uniandes.edu.co.proyecto.repositorio;
 
-import java.time.LocalDate;
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 import uniandes.edu.co.proyecto.modelo.OrdenDeCompra;
 
-public interface OrdenDeCompraRepository extends JpaRepository<OrdenDeCompra, Integer> {
+public interface OrdenDeCompraRepository extends JpaRepository<OrdenDeCompra, Integer>{
 
-    @Query(value = "SELECT * FROM orden_de_compra", nativeQuery=true)
+    @Query(value = "SELECT * FROM ORDEN_DE_COMPRA", nativeQuery = true)
     Collection<OrdenDeCompra> darOrdenesDeCompra();
-
-    @Query(value = "SELECT * FROM orden_de_compra WHERE numero= :numero", nativeQuery=true)
-        OrdenDeCompra darOrdenDeCompra(@Param("numero") Integer numero);
-
-        
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO orden_de_compra(numero, fecha_entrega, estado, fecha_creacion, id_sucursal, nit_proveedor) VALUES(secuencia_orden_de_compra.nextval, :fecha_entrega, :estado, :fecha_creacion, :id_sucursal, :nit_proveedor)", nativeQuery = true)
-    void insertarOrdenDeCompra(@Param("fecha_entrega") LocalDate fechaEntrega, @Param("estado") String estado, @Param("fecha_creacion") LocalDate fechaCreacion, @Param("id_sucursal") Integer idSucursal, @Param("nit_proveedor") String nitProveedor);
-
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE orden_de_compra SET estado = :estado WHERE numero = :numero", nativeQuery = true)
-    void actualizarEstadoOrdenDeCompra(@Param("numero") Integer numero, @Param("estado") String estado);
-
 }
