f4d2323691a7fe74db9920fa3946927b5b9f9ac9
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-11-01 11:55:11 -0500

    queryCostoPromedio

--- src/main/java/uniandes/edu/co/proyecto/repositorio/ProductoEnBodegaRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/ProductoEnBodegaRepository.java
@@ -1,8 +1,10 @@
 package uniandes.edu.co.proyecto.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import uniandes.edu.co.proyecto.modelo.ProductoEnBodega;
 import uniandes.edu.co.proyecto.modelo.ProductoEnBodegaPK;
@@ -12,4 +14,15 @@ public interface ProductoEnBodegaRepository extends JpaRepository<ProductoEnBode
     // Consulta nativa para obtener un producto específico en una bodega específica
     @Query(value = "SELECT * FROM PRODUCTO_EN_BODEGA WHERE IDENTIFICADOR_PRODUCTO = :idProducto AND ID_BODEGA = :idBodega", nativeQuery = true)
     ProductoEnBodega findByProductoYBodega(@Param("idProducto") Integer idProducto, @Param("idBodega") Integer idBodega);
+
+    //NEW NEW NEW 
+    //Hacer calculo del costo promedio con query SQL
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE PRODUCTO_EN_BODEGA \r\n" +
+                   "SET CANTIDAD_EN_BODEGA = CANTIDAD_EN_BODEGA + :cantidadIngresada, \r\n" +
+                   "COSTO_PROMEDIO = ((COSTO_PROMEDIO * CANTIDAD_EN_BODEGA) + (:precioUnitario * :cantidadIngresada)) / (CANTIDAD_EN_BODEGA + :cantidadIngresada) " +
+                   "WHERE IDENTIFICADOR_PRODUCTO = :idProducto AND ID_BODEGA = :idBodega", nativeQuery = true)
+    void actualizarCostoPromedioYcantidad(@Param("idProducto") Integer idProducto, @Param("idBodega") Integer idBodega, @Param("precioUnitario") Double precioUnitario, @Param("cantidadIngresada") Integer cantidadIngresada);
 }
+
