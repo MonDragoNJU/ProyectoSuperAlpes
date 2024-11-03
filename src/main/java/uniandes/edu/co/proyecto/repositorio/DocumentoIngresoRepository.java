295d8e48671af46cbebc46d0f0bae511c9a90f46
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-11-03 00:01:46 -0500

    FOR UPDATE

--- src/main/java/uniandes/edu/co/proyecto/repositorio/DocumentoIngresoRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/DocumentoIngresoRepository.java
@@ -31,6 +31,7 @@ public interface DocumentoIngresoRepository extends JpaRepository<DocumentoIngre
     @Query(value = "INSERT INTO documento_ingreso(id, fecha_ingreso, id_bodega, numero_orden_de_compra) VALUES (secuencia_documento_ingreso.nextval, :fechaIngreso, :idBodega, :numeroOrdenCompra)", nativeQuery = true)
     void insertarDocumentoIngreso(@Param("fechaIngreso") LocalDate fechaIngreso, @Param("idBodega") Long idBodega, @Param("numeroOrdenCompra") Long numeroOrdenCompra);
 
+    //NEW NEW FOR UPDATE
     @Query(value = "SELECT DOCUMENTO_INGRESO.ID AS id_documento, DOCUMENTO_INGRESO.FECHA_INGRESO AS fecha_ingreso, SUCURSAL.NOMBRE AS nombre_sucursal, BODEGA.NOMBRE AS nombre_bodega, PROVEEDOR.NOMBRE AS nombre_proveedor \r\n" +
         "FROM DOCUMENTO_INGRESO \r\n" +
         "JOIN ORDEN_DE_COMPRA ON DOCUMENTO_INGRESO.NUMERO_ORDEN_DE_COMPRA = ORDEN_DE_COMPRA.NUMERO \r\n" +
@@ -38,7 +39,8 @@ public interface DocumentoIngresoRepository extends JpaRepository<DocumentoIngre
         "JOIN BODEGA ON DOCUMENTO_INGRESO.ID_BODEGA = BODEGA.ID \r\n" +
         "JOIN PROVEEDOR ON ORDEN_DE_COMPRA.NIT_PROVEEDOR = PROVEEDOR.NIT \r\n" +
         "WHERE ORDEN_DE_COMPRA.ID_SUCURSAL = :idSucursal AND DOCUMENTO_INGRESO.ID_BODEGA = :idBodega AND DOCUMENTO_INGRESO.FECHA_INGRESO >= :fechaLimite " +
-        "ORDER BY DOCUMENTO_INGRESO.FECHA_INGRESO DESC", nativeQuery = true)
+        "ORDER BY DOCUMENTO_INGRESO.FECHA_INGRESO DESC \r\n" +
+        "FOR UPDATE", nativeQuery = true)
         //Usamos un map por lo que no solo regresa objetos de tipo DocumentoIngreso, sino otros elementos 
     List<Map<String, Object>> obtenerDocumentosIngreso(@Param("idSucursal") Long idSucursal, @Param("idBodega") Long idBodega, @Param("fechaLimite") LocalDate fechaLimite);
 
